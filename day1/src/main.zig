fcd32940881722a2df5d31b99f8fb5460fffb35c
ArmanLk <74207911+ArmanLK@users.noreply.github.com>
2023-12-03 20:55:32 +0330

    refactored day1

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -1,11 +1,6 @@
 const std = @import("std");
 const stdin = std.io.getStdIn();
 
-pub fn main() !void {
-    //try part1();
-    try part2();
-}
-
 const NUMBERS = std.ComptimeStringMap(u32, .{
     .{ "one", 1 },
     .{ "two", 2 },
@@ -18,56 +13,62 @@ const NUMBERS = std.ComptimeStringMap(u32, .{
     .{ "nine", 9 },
 });
 
-pub fn part1() !void {
+pub fn main() !void {
     var buf: [64]u8 = undefined;
-    var sum: usize = 0;
+    var sum1: usize = 0;
+    var sum2: usize = 0;
+
     while (stdin.reader().readUntilDelimiter(&buf, '\n') catch null) |line| {
-        var first: u32 = 0;
-        var last: u32 = 0;
-        for (line) |ch| {
-            if (parsePart1(ch)) |num| {
-                if (first == 0) {
-                    first = num;
-                }
-                last = num;
-            }
-        }
-        sum += last + first * 10;
+        sum1 += part1(line);
+        sum2 += part2(line);
     }
 
-    std.debug.print("{}\n", .{sum});
+    std.debug.print("part1: {}\npart2: {}\n", .{ sum1, sum2 });
+}
+
+pub fn part1(line: []const u8) u32 {
+    var first: u32 = 0;
+    var last: u32 = 0;
+
+    for (line) |ch| {
+        if (parsePart1(ch)) |num| {
+            if (first == 0) {
+                first = num;
+            }
+            last = num;
+        }
+    }
+
+    return last + first * 10;
 }
 
 fn parsePart1(ch: u8) ?u32 {
     for ([_]u8{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, 0..) |num, i| {
         if (ch == num) return @intCast(i);
     }
+
     return null;
 }
 
-pub fn part2() !void {
-    var buf: [64]u8 = undefined;
-    var sum: usize = 0;
-    while (stdin.reader().readUntilDelimiter(&buf, '\n') catch null) |line| {
-        var first: u32 = 0;
-        var last: u32 = 0;
-        for (line, 0..) |ch, i| {
-            if (parsePart1(ch)) |num| {
-                if (first == 0) {
-                    first = num;
-                }
-                last = num;
-            } else if (parsePart2(line, i)) |num| {
-                if (first == 0) {
-                    first = num;
-                }
-                last = num;
+pub fn part2(line: []const u8) u32 {
+    var first: u32 = 0;
+    var last: u32 = 0;
+
+    for (line, 0..) |ch, i| {
+        if (parsePart1(ch)) |num| {
+            if (first == 0) {
+                first = num;
             }
+            last = num;
+        } else if (parsePart2(line, i)) |num| {
+            if (first == 0) {
+                first = num;
+            }
+            last = num;
         }
-        sum += last + first * 10;
     }
 
-    std.debug.print("{}\n", .{sum});
+    return last + first * 10;
 }
 fn parsePart2(line: []const u8, i: usize) ?u32 {
     const len = line.len;
